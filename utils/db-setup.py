34edb823c0be8c88724e79c70f682fff9c11f086
hrs201 <haarisrahmanyar@gmail.com>
2025-09-24 20:07:59 -0400

    Initial commit - initialize SQLite database and create campaigns table

---
+++ utils/db-setup.py
@@ -0,0 +1,21 @@
+import sqlite3
+import pandas as pd
+
+connect = sqlite3.connect('genAI-marketing-tool.db')
+cursor = connect.cursor()
+
+cursor.execute('''
+            CREATE TABLE IF NOT EXISTS campaigns(
+               id INTEGER PRIMARY KEY,
+               name TEXT,
+               impressions INTEGER,
+               clicks INTEGER,
+               conversions INTEGER
+               )
+''')
+
+
+df = pd.read_sql_query("SELECT * FROM campaigns", connect)
+print(df)
+
+
