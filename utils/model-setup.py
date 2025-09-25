73badf7979c3c42c2582d354814facd4a6cb5423
hrs201 <haarisrahmanyar@gmail.com>
2025-09-24 20:24:36 -0400

    Created model-setup.py - added T5-based text generation pipeline for caption creation

---
+++ utils/model-setup.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+
+def get_t5_generator():
+    return pipeline("text2text-generation", model="t5-base")
+
+generator = get_t5_generator()
+
+def generate_caption(product_name):
+    prompt = f"Write a catchy Instagram caption for a product called '{product_name}'"
+    result = generator(prompt, max_length=50)
+    return result[0]['generated_text']
+
+print(generate_caption("Wonder Bread Whole Wheat"))
